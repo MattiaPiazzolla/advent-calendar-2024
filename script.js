a1d2985b9ac6a5f5e9ed4292756f91675e569963
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-12-30 15:58:39 +0100

    refactor calendar generation and enhance styling; implement responsive design and improve day box interactions

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
-
-// recover the calendar div from the DOM
+// Recover the calendar div from the DOM
 const calendar = document.querySelector('.calendar');
+
+// Base path for images
+const basePath = './images/icons/';
+const extension = '.png';
 
 // Generate the calendar
 for (let i = 1; i <= 25; i++) {
     // Define the day box
     const dayBox = document.createElement('div');
-    dayBox.classList.add('day-box');
+    if (i === 25) {
+        dayBox.classList.add('last-day');
+    } else {
+        dayBox.classList.add('day-box');
+    }
+    
+    
+    // Allign the values of the source array to the day number
+    const iconData = source[i - 1]; 
+    
+    // Create the image element
+    const img = document.createElement('img');
+    img.src = `${basePath}${iconData.icon}${extension}`;
+    img.alt = `Icon for day ${i}`;
+    img.classList.add('day-icon');
+
+    // Append the image to the day box
+    dayBox.appendChild(img);
+    
+    
     
     // Define the day number
-    dayBox.textContent = i;
+    const dayNumber = document.createElement('span');
+    dayNumber.textContent = i;
+
+    // Append the day number to the day box
+    dayBox.appendChild(dayNumber);
 
     // Append the day box to the calendar
     calendar.appendChild(dayBox);
 }
-
